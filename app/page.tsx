bb1c1bef9db94c12a02c11f6be3664f491f31ce3
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-05-27 01:14:58 +0530

    Hero Page is underconstruction

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+// import Image from "next/image";
+
+import Hero from "@/components/main/Hero";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col h-[850px] gap-20">
+      <Hero />
+      </div>
+      </main>
+  );
+}
+
+//https://youtu.be/0siQoaXMmzk?si=lpdUI55IELYumMHO&t=969
